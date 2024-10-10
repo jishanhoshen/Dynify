d19ac63b82f0aba0308dd39b25dee67f180679ca
jishanhoshen <jishanhoshenjibon@gmail.com>
2024-10-10 22:26:04 +0600

    update styole

--- app/Views/home.php
+++ app/Views/home.php
@@ -10,11 +10,10 @@
   </div>
 </div>
 
-
-<section id="gpr" style="padding: 200px 0px 100px">
+<section class="section2" id="gpr">
   <div class="container">
     <div class="row">
-      <div class="col-12 col-md-6 d-flex justify-content-end">
+      <div class="col-12 col-md-6 left">
         <img src="public/images/section2.png" alt="" class="img-fluid" width="70%">
       </div>
       <div class="col-12 col-md-6 d-flex justify-content-center flex-column">
@@ -25,51 +24,7 @@
   </div>
 </section>
 
-<section class="bullet-list d-none" style="padding: 100px 0;">
-  <div class="container">
-    <div class="d-flex justify-content-center">
-      <div class="bullet-list-item me-5">
-        <div class="d-flex">
-          <img src="public/images/Rectangle.svg">
-          <div class="ps-2 fw-bold">Rebar Locating</div>
-        </div>
-        <div class="d-flex">
-          <img src="public/images/Rectangle.svg">
-          <div class="ps-2 fw-bold">Post Tension Cable Mapping</div>
-        </div>
-        <div class="d-flex">
-          <img src="public/images/Rectangle.svg">
-          <div class="ps-2 fw-bold">Structural Analysis</div>
-        </div>
-        <div class="d-flex">
-          <img src="public/images/Rectangle.svg">
-          <div class="ps-2 fw-bold">Concrete Thickness</div>
-        </div>
-      </div>
-      <div class="bullet-list-item">
-        <div class="d-flex">
-          <img src="public/images/Rectangle.svg">
-          <div class="ps-2 fw-bold">Slab on Grade Applications</div>
-        </div>
-        <div class="d-flex">
-          <img src="public/images/Rectangle.svg">
-          <div class="ps-2 fw-bold">Shallow Utility Locating </div>
-        </div>
-        <div class="d-flex">
-          <img src="public/images/Rectangle.svg">
-          <div class="ps-2 fw-bold">Conduit Mapping</div>
-        </div>
-        <div class="d-flex">
-          <img src="public/images/Rectangle.svg">
-          <div class="ps-2 fw-bold ">Ground Penetrating Radar</div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
-
-<section class="section2">
+<section class="section3">
   <div class="wrap">
     <h4>IN PURSUIT OF 100%</h4>
     <h3>SUBSURFACE</h3>
@@ -78,7 +33,7 @@
   </div>
 </section>
 
-<section class="service-list-area" style="padding: 200px 0px 100px" id="services">
+<section class="section4 service-list-area" style="padding: 200px 0px 100px" id="services">
   <div class="container">
     <div class="row service-list ">
       <div class="col-12 col-lg-3 service">
@@ -133,47 +88,47 @@
   </div>
 </section>
 
-<section class="section3" style="padding-bottom: 100px">
+<section class="section5" style="padding-bottom: 100px">
   <div class="container">
     <div class="row">
       <div class="col-12 col-md-6">
         <img src="public/images/7272030.jpg" alt="" class="img-fluid" style="width:100%; height: 280px; object-fit:cover; border-radius:10px">
-        <h4 class="pt-3">Ground Penetrating Radar</h4>
+        <h4 class="pt-3 long-title">Ground Penetrating Radar</h4>
         <p class="pt-2">GPR scanners emit radio waves into concrete slabs and detect the interactions between these waves and any subsurface objects to create a readout of hyperbolas that vary in size and shape depending on the type of material detected. GPRS’ SIM-certified Project Managers interpret this data to tell you what was located and estimate its depth within the slab.</p>
       </div>
       <div class="col-12 col-md-6">
         <img src="public/images/e5gdhs5hst.jpeg" alt="" class="img-fluid" style="width:100%; height: 280px; object-fit:cover; border-radius:10px">
-        <h4 class="pt-3">Concrete Thickness</h4>
+        <h4 class="pt-3 long-title">Concrete Thickness</h4>
         <p class="pt-2">GPRS Project Managers provide GPR scanning services to determine key slab information for structural engineers, including concrete cover and overlay thickness, concrete thickness, and even dowel placement. We aren’t limited by the size or scope of your site; we have the training and the equipment to fully evaluate your concrete structure.</p>
       </div>
       <div class="col-12 col-md-6">
         <img src="public/images/Slab on Grade Application.jpg" alt="" class="img-fluid" style="width:100%; height: 280px; object-fit:cover; border-radius:10px">
-        <h4 class="pt-3">Slab on Grade</h4>
+        <h4 class="pt-3 long-title">Slab on Grade</h4>
         <p class="pt-2">Ground penetrating radar’s ability to visualize what’s inside and under concrete slab-on-grade is one of its biggest advantages over X-Ray scanning technology. Because GPR only needs access to one side of a concrete slab or structure to scan the material for anomalies such as embedded conduit, it can evaluate slab-on-grade.</p>
       </div>
       <div class="col-12 col-md-6">
         <img src="public/images/Shallow Utility Locating.jpg" alt="" class="img-fluid" style="width:100%; height: 280px; object-fit:cover; border-radius:10px">
-        <h4 class="pt-3">Shallow Utility Locating</h4>
+        <h4 class="pt-3 long-title">Shallow Utility Locating</h4>
         <p class="pt-2">It’s not just the utilities within your concrete slab that you need to worry about – if it’s a slab-on-grade, you also need to avoid lines buried in the soil below. GPRS Project Managers utilize both ground penetrating radar and electromagnetic (EM) locating to provide you with a 99.8%+ accurate picture of the infrastructure within and below your concrete slab.</p>
       </div>
       <div class="col-12 col-md-6">
         <img src="public/images/Conduit Mapping.png" alt="" class="img-fluid" style="width:100%; height: 280px; object-fit:cover; border-radius:10px">
-        <h4 class="pt-3">Conduit Mapping</h4>
+        <h4 class="pt-3 long-title">Conduit Mapping</h4>
         <p class="pt-2">We know you can’t risk severing an electrical conduit while coring or cutting through concrete. We mark our GPR findings directly on your slab so you know where all subsurface obstructions are buried and where you can safely cut or core.</p>
       </div>
       <div class="col-12 col-md-6">
         <img src="public/images/Rebar Locating.jpg" alt="" class="img-fluid" style="width:100%; height: 280px; object-fit:cover; border-radius:10px">
-        <h4 class="pt-3">Rebar Locating</h4>
+        <h4 class="pt-3 long-title">Rebar Locating</h4>
         <p class="pt-2">Damaged rebar will cost you tens of thousands of dollars to repair, and that’s not counting any additional structural damage or injuries that occur when the support is damaged. GPRS Project Managers are specially trained to use GPR to locate and map the rebar within your slab or concrete structure.</p>
       </div>
       <div class="col-12 col-md-6">
         <img src="public/images/Post Tension Cable Mapping.png" alt="" class="img-fluid" style="width:100%; height: 280px; object-fit:cover; border-radius:10px">
-        <h4 class="pt-3">Post Tension Cable Mapping</h4>
+        <h4 class="pt-3 long-title">Post Tension Cable Mapping</h4>
         <p class="pt-2">It’s not just the utilities within your concrete slab that you need to worry about – if it’s a slab-on-grade, you also need to avoid lines buried in the soil below. GPRS Project Managers utilize both ground penetrating radar and electromagnetic (EM) locating to provide you with a 99.8%+ accurate picture of the infrastructure within and below your concrete slab.</p>
       </div>
       <div class="col-12 col-md-6">
         <img src="public/images/Slab on Grade Application.jpg" alt="" class="img-fluid" style="width:100%; height: 280px; object-fit:cover; border-radius:10px">
-        <h4 class="pt-3">Structural Analysis</h4>
+        <h4 class="pt-3 long-title">Structural Analysis</h4>
         <p class="pt-2">It’s not just the utilities within your concrete slab that you need to worry about – if it’s a slab-on-grade, you also need to avoid lines buried in the soil below. GPRS Project Managers utilize both ground penetrating radar and electromagnetic (EM) locating to provide you with a 99.8%+ accurate picture of the infrastructure within and below your concrete slab.</p>
       </div>
     </div>
@@ -181,7 +136,7 @@
 </section>
 
 
-<section class="get-quote" id="get-quote">
+<section class="section6 get-quote" id="get-quote">
   <div class="container">
     <div class="row">
       <div class="col-12 col-lg-6 ps-lg-5 pb-5 pb-lg-0">
@@ -196,21 +151,21 @@
           <ul class="list-group list-group-flush">
             <li class="list-group-item border-0">
               <svg xmlns="http://www.w3.org/2000/svg" width="22" height="23" viewBox="0 0 22 23" fill="none">
-                <path d="M20.8881 16.4562V19.4562C20.8892 19.7347 20.8322 20.0104 20.7206 20.2656C20.6091 20.5208 20.4454 20.7498 20.2402 20.9381C20.035 21.1264 19.7927 21.2697 19.5289 21.3589C19.265 21.4482 18.9855 21.4813 18.7081 21.4562C15.631 21.1219 12.6751 20.0704 10.0781 18.3862C7.66194 16.8509 5.61345 14.8024 4.07812 12.3862C2.38809 9.77743 1.33636 6.80722 1.00812 3.71623C0.983127 3.4397 1.01599 3.16099 1.10462 2.89785C1.19324 2.63472 1.33569 2.39292 1.52288 2.18785C1.71008 1.98278 1.93792 1.81894 2.19191 1.70675C2.44589 1.59456 2.72046 1.53649 2.99812 1.53623H5.99812C6.48342 1.53145 6.95391 1.70331 7.32188 2.01976C7.68985 2.33622 7.93019 2.77568 7.99812 3.25623C8.12474 4.2163 8.35957 5.15896 8.69812 6.06623C8.83266 6.42415 8.86178 6.81314 8.78202 7.18711C8.70227 7.56108 8.51698 7.90434 8.24812 8.17623L6.97812 9.44623C8.40167 11.9498 10.4746 14.0227 12.9781 15.4462L14.2481 14.1762C14.52 13.9074 14.8633 13.7221 15.2372 13.6423C15.6112 13.5626 16.0002 13.5917 16.3581 13.7262C17.2654 14.0648 18.2081 14.2996 19.1681 14.4262C19.6539 14.4948 20.0975 14.7394 20.4146 15.1137C20.7318 15.488 20.9003 15.9658 20.8881 16.4562Z" stroke="#ffa7a7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M20.8881 16.4562V19.4562C20.8892 19.7347 20.8322 20.0104 20.7206 20.2656C20.6091 20.5208 20.4454 20.7498 20.2402 20.9381C20.035 21.1264 19.7927 21.2697 19.5289 21.3589C19.265 21.4482 18.9855 21.4813 18.7081 21.4562C15.631 21.1219 12.6751 20.0704 10.0781 18.3862C7.66194 16.8509 5.61345 14.8024 4.07812 12.3862C2.38809 9.77743 1.33636 6.80722 1.00812 3.71623C0.983127 3.4397 1.01599 3.16099 1.10462 2.89785C1.19324 2.63472 1.33569 2.39292 1.52288 2.18785C1.71008 1.98278 1.93792 1.81894 2.19191 1.70675C2.44589 1.59456 2.72046 1.53649 2.99812 1.53623H5.99812C6.48342 1.53145 6.95391 1.70331 7.32188 2.01976C7.68985 2.33622 7.93019 2.77568 7.99812 3.25623C8.12474 4.2163 8.35957 5.15896 8.69812 6.06623C8.83266 6.42415 8.86178 6.81314 8.78202 7.18711C8.70227 7.56108 8.51698 7.90434 8.24812 8.17623L6.97812 9.44623C8.40167 11.9498 10.4746 14.0227 12.9781 15.4462L14.2481 14.1762C14.52 13.9074 14.8633 13.7221 15.2372 13.6423C15.6112 13.5626 16.0002 13.5917 16.3581 13.7262C17.2654 14.0648 18.2081 14.2996 19.1681 14.4262C19.6539 14.4948 20.0975 14.7394 20.4146 15.1137C20.7318 15.488 20.9003 15.9658 20.8881 16.4562Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
               </svg>
               <span>678-866-4177</span>
             </li>
             <li class="list-group-item border-0">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
-                <path d="M4 4.5H20C21.1 4.5 22 5.4 22 6.5V18.5C22 19.6 21.1 20.5 20 20.5H4C2.9 20.5 2 19.6 2 18.5V6.5C2 5.4 2.9 4.5 4 4.5Z" stroke="#ffa7a7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                <path d="M22 6.5L12 13.5L2 6.5" stroke="#ffa7a7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M4 4.5H20C21.1 4.5 22 5.4 22 6.5V18.5C22 19.6 21.1 20.5 20 20.5H4C2.9 20.5 2 19.6 2 18.5V6.5C2 5.4 2.9 4.5 4 4.5Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M22 6.5L12 13.5L2 6.5" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
               </svg>
               <span>info@corepro.net</span>
             </li>
             <li class="list-group-item border-0">
               <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
-                <path d="M21 1L10 12" stroke="#ffa7a7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                <path d="M21 1L14 21L10 12L1 8L21 1Z" stroke="#ffa7a7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M21 1L10 12" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M21 1L14 21L10 12L1 8L21 1Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
               </svg>
               <span>address</span>
             </li>
@@ -264,6 +219,9 @@
   // AJAX form submission
   $('#contactForm').on('submit', function(e) {
     e.preventDefault();
+
+    $('#contactForm [type="submit"]').prop('disabled', true);
+
     var formData = {
       name: $('#name').val().trim(),
       email: $('#email').val().trim(),
@@ -349,9 +307,11 @@
           } else {
             toastr.error(res.message);
           }
+          $('#contactForm [type="submit"]').prop('disabled', false);
         },
         error: function(xhr, status, error) {
           console.log(xhr.responseText);
+          $('#contactForm [type="submit"]').prop('disabled', false);
         }
       });
     }
